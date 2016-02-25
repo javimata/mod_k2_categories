52625ab9389aa19cd42ff10b7e59b1a08a79fd2e
Javier Mata <javimata@gmail.com>
2016-02-25 16:16:37 -0600

    Upload files

---
+++ mod_k2_categories/helper.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * @version		2.6.x
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+require_once (JPATH_SITE.DS.'components'.DS.'com_k2'.DS.'helpers'.DS.'route.php');
+require_once (JPATH_SITE.DS.'components'.DS.'com_k2'.DS.'helpers'.DS.'utilities.php');
+
+class modK2CategoriesHelper
+{
+
+	public static function getItems(&$params, $format = 'html')
+	{
+
+		jimport('joomla.filesystem.file');
+		$mainframe = JFactory::getApplication();
+		$limit = $params->get('itemCount', 5);
+		$cid = $params->get('category_id', NULL);
+		$ordering = $params->get('itemsOrdering', '');
+		$componentParams = JComponentHelper::getParams('com_k2');
+		$limitstart = JRequest::getInt('limitstart');
+
+		$user = JFactory::getUser();
+		$aid = $user->get('aid');
+		$db = JFactory::getDBO();
+
+		$jnow = JFactory::getDate();
+		$now =  K2_JVERSION == '15'?$jnow->toMySQL():$jnow->toSql();
+		$nullDate = $db->getNullDate();
+
+
+		// Inicia Query
+		$query  = "SELECT c.*";
+		$query .= " FROM #__k2_categories as c";
+		$query .= " WHERE c.published = 1";
+
+		if (!is_null($cid))
+		{
+			if (is_array($cid))
+			{
+				$count = 0;
+				foreach ($cid as $cat) {
+					if ( $count >= 1 ) { $sep = " OR "; } else { $sep = " AND "; }
+					$query .= $sep ."c.id = " . $cat;
+					$count++;
+				}
+
+			}
+			else
+			{
+				$query .= " AND c.id = " . $cat;
+			}
+		}
+
+
+		switch ($ordering)
+		{
+
+			case 'date' :
+				$orderby = 'c.id ASC';
+				break;
+
+			case 'name' :
+				$orderby = 'c.name ASC';
+				break;
+
+
+			case 'order' :
+				$orderby = 'c.ordering ASC';
+				break;
+
+			case 'rand' :
+				$orderby = 'RAND()';
+				break;
+
+			default :
+				$orderby = 'c.id DESC';
+				break;
+		}
+
+		$query .= " ORDER BY ".$orderby;
+		$db->setQuery($query, 0, $limit);
+		$items = $db->loadObjectList();
+
+		$model = K2Model::getInstance('Item', 'K2Model');
+
+		if (count($items))
+		{
+
+			foreach ($items as $item)
+			{
+			    $item->event = new stdClass;
+
+				//Clean title
+				$item->title = JFilterOutput::ampReplace($item->title);
+
+				//Images
+				if ($params->get('itemImage'))
+				{
+
+					$date = JFactory::getDate($item->modified);
+					$timestamp = '?t='.$date->toUnix();
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_XS.jpg'))
+					{
+						$item->imageXSmall = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_XS.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageXSmall .= $timestamp;
+						}
+					}
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_S.jpg'))
+					{
+						$item->imageSmall = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_S.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageSmall .= $timestamp;
+						}
+					}
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_M.jpg'))
+					{
+						$item->imageMedium = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_M.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageMedium .= $timestamp;
+						}
+					}
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_L.jpg'))
+					{
+						$item->imageLarge = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_L.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageLarge .= $timestamp;
+						}
+					}
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_XL.jpg'))
+					{
+						$item->imageXLarge = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_XL.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageXLarge .= $timestamp;
+						}
+					}
+
+					if (JFile::exists(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.md5("Image".$item->id).'_Generic.jpg'))
+					{
+						$item->imageGeneric = JURI::base(true).'/media/k2/items/cache/'.md5("Image".$item->id).'_Generic.jpg';
+						if ($componentParams->get('imageTimestamp'))
+						{
+							$item->imageGeneric .= $timestamp;
+						}
+					}
+
+					$image = 'image'.$params->get('itemImgSize', 'Small');
+					if (isset($item->$image))
+						$item->image = $item->$image;
+
+				}
+
+				//Read more link
+				$item->link = urldecode(JRoute::_(K2HelperRoute::getItemRoute($item->id.':'.urlencode($item->alias))));
+
+				//Category link
+				// $item->categoryLink = urldecode(JRoute::_(K2HelperRoute::getCategoryRoute($item->id)));
+				$item->categoryLink = urldecode(JRoute::_(K2HelperRoute::getCategoryRoute($item->id.':'.urlencode($item->alias))));
+
+				$item->link = JURI::BASE(true) . '/' . $item->alias;
+
+				// Introtext
+				$item->text = '';
+				if ($params->get('itemIntroText'))
+				{
+					// Word limit
+					$item->text .= $item->description;
+				}
+
+
+                // Restore the intotext variable after plugins execution
+                $item->introtext = $item->text;
+
+
+				$rows[] = $item;
+			}
+			return $rows;
+
+		}
+
+	}
+
+}
---
+++ mod_k2_categories/tmpl/Default/default.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @version		2.6.x
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+
+<div id="k2Categories<?php echo $module->id; ?>" class="k2CategoriesBlock<?php if($params->get('moduleclass_sfx')) echo ' '.$params->get('moduleclass_sfx'); ?>">
+
+
+<?php
+
+	$cols = $params->get("colsCount",4);
+	$classList ="";
+
+	switch ($params->get('listType'))
+	{
+
+		case 'div' :
+			$ancho_cada  = 100 / $cols;
+			$inicia      = '<div>';
+			$cierra      = '</div>';
+			$cada_inicia = '<div style="width:' . round ($ancho_cada) . '%" class="itemList [CLASSLIST]">';
+			$cada_cierra = '</div>';
+			break;
+
+		case 'bootstrap' :
+			$ancho_cada  = 12 / $cols;
+			$inicia      = '<div class="row">';
+			$cierra      = '</div>';
+			$cada_inicia = '<div class="col-md-' . round ($ancho_cada) . ' itemList [CLASSLIST]">';
+			$cada_cierra = '</div>';
+			break;
+
+		default :
+			$inicia      = '<ul class="listCategories">';
+			$cierra      = '</ul>';
+			$cada_inicia = '<li style="width:' . round ($ancho_cada) . '%" class="itemList [CLASSLIST]">';
+			$cada_cierra = '</li>';
+			break;
+	}
+?>
+
+
+  <?php echo $inicia; ?>
+
+    <?php foreach ($items as $key=>$item):	?>
+
+    	<?php
+    	$classList = ($key%2) ? "odd" : "even"; 
+    	if(count($items)==$key+1) $classList .= ' lastItem';
+    	?>
+
+    	<?php echo str_replace("[CLASSLIST]", $classList, $cada_inicia); ?>
+
+			<?php if($params->get('itemTitle')!=0): ?>
+			<div class="itemTitle">
+				<?php if ($params->get('itemTitle')==2): ?>
+				<a class="linkItemTitle" href="<?php echo $item->link; ?>">
+					<?php echo $item->name; ?>
+				</a>
+				<?php else: ?>
+					<?php echo $item->name; ?>					
+				<?php endif; ?>
+			</div>
+			<?php endif; ?>
+
+			<?php if($params->get('itemImage') || $params->get('itemIntroText')): ?>
+			<div class="ItemIntrotext">
+				<?php if($params->get('itemImage') && isset($item->image)): ?>
+				<a class="moduleItemImage" href="<?php echo $item->link; ?>" title="<?php echo JText::_('K2_CONTINUE_READING'); ?> &quot;<?php echo K2HelperUtilities::cleanHtml($item->name); ?>&quot;">
+				<img src="<?php echo $item->image; ?>" alt="<?php echo K2HelperUtilities::cleanHtml($item->name); ?>"/>
+				</a>
+				<?php endif; ?>
+
+				<?php if($params->get('itemIntroText')): ?>
+				<?php echo $item->introtext; ?>
+				<?php endif; ?>
+			</div>
+			<?php endif; ?>
+
+			<?php if($params->get('itemReadMore')): ?>
+			<a class="ItemReadMore" href="<?php echo $item->link; ?>">
+				<?php echo JText::_('K2_READ_MORE'); ?>
+			</a>
+			<?php endif; ?>
+
+    	<?php echo $cada_cierra; ?>
+    <?php endforeach; ?>
+  
+  <?php echo $cierra; ?>
+
+</div>
